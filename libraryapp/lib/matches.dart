6f2dad43fc2388ed4925259ad7ad63f5c640c10d
Jonas Arvidsson <jonas.arvidsson0@gmail.com>
2017-10-13 16:15:04 +0200

    list of events added

--- libraryapp/lib/matches.dart
+++ libraryapp/lib/matches.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import './screens/create_event.dart' as create_event;
 
 class Matches extends StatelessWidget {
   @override
@@ -10,7 +11,18 @@ class Matches extends StatelessWidget {
     return new AppBar(
         title: new Text("Matches"),
         centerTitle: true,
-        backgroundColor: Colors.green);
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.add),
+            tooltip: "Create Event",
+            onPressed: () {
+                 Navigator.of(context).push(new MaterialPageRoute<Null>(
+                      builder: (BuildContext context) =>
+                          new create_event.CreateEvent(),
+                    ));
+            })
+        ]);
   }
 
   Widget buildBody(BuildContext context) {
--- libraryapp/lib/tabs/upcoming.dart
+++ libraryapp/lib/tabs/upcoming.dart
@@ -1,8 +1,67 @@
 import "package:flutter/material.dart";
+import 'dart:io';
 
 class Upcoming extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
-    return new Container(child: new Center(child: new Text("UPCOMING")));
-  }
+    return new Container(
+      child: new ListView(
+        padding: new EdgeInsets.all(8.0),
+          children: <Widget>[
+            //Column which contains all rows
+            new Column(
+              children: [
+                //First row
+                new Container(
+                 margin: new EdgeInsets.only(bottom: 10.0), 
+                 height: 100.0,
+                 decoration: new BoxDecoration(color: Colors.green[400],
+                  boxShadow: <BoxShadow>[
+                    new BoxShadow (
+                      color: const Color(0xcc000000),
+                      offset: new Offset(0.0, 2.0),
+                      blurRadius: 4.0,
+                    ),
+                  ], 
+                    borderRadius: new BorderRadius.all(
+                  const Radius.circular(0.0),
+                    ), ), 
+                  child: new Row(
+                    children: [
+                      new Image.asset(
+                        'images/fox.jpg',
+                        height: 100.0,
+                        fit: BoxFit.cover,
+                            ),
+                          ]
+                        ),
+                      ),
+                    //second row
+                       new Container(
+                 margin: new EdgeInsets.only(bottom: 10.0), 
+                  color: Colors.green[200],
+                  child: new Row(
+                    children: [
+                      new Image.asset(
+                        'images/fox.jpg',
+                        height: 100.0,
+                        fit: BoxFit.contain,
+
+                            ),
+                          ]
+                        ),
+                      ),
+                      //end of row
+                    ]),
+                  ],
+                )
+              );
+            }
+          }
+
+
+Widget getEvents(){
+
 }
