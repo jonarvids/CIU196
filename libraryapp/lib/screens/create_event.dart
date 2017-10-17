524ec55e33ba713862a5be935824e7a10f0ef822
Alotte <alma.ottedag@gmail.com>
2017-10-17 16:39:51 +0200

    Added a banner image
    Still need to enter event date

--- libraryapp/lib/screens/create_event.dart
+++ libraryapp/lib/screens/create_event.dart
@@ -1,19 +1,21 @@
+import "package:flutter/material.dart";
 import 'dart:async';
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/services.dart';
 
 class CreateEvent extends StatefulWidget {
   @override
   createState() => new CreateEventState();
 }
 
-
 class EventData {
   String description = '';
   String title = '';
+  File imageFile;
 }
 
 class CreateEventState extends State<CreateEvent> {
-
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   EventData event = new EventData();
 
@@ -23,9 +25,8 @@ class CreateEventState extends State<CreateEvent> {
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
 
   void showInSnackBar(String value) {
-    _scaffoldKey.currentState.showSnackBar(new SnackBar(
-        content: new Text(value)
-    ));
+    _scaffoldKey.currentState
+        .showSnackBar(new SnackBar(content: new Text(value)));
   }
 
   //MARK: Form Submission
@@ -43,8 +44,7 @@ class CreateEventState extends State<CreateEvent> {
   //MARK: Validation messages
   String _validateTitle(String value) {
     _formWasEdited = true;
-    if (value.isEmpty)
-      return 'Title is required.';
+    if (value.isEmpty) return 'Title is required.';
     final RegExp nameExp = new RegExp(r'^[A-Za-z ]+$');
     if (!nameExp.hasMatch(value))
       return 'Please enter only alphabetical characters.';
@@ -55,84 +55,167 @@ class CreateEventState extends State<CreateEvent> {
 
   Future<bool> _warnUserAboutInvalidData() async {
     final FormState form = _formKey.currentState;
-    if (form == null || !_formWasEdited || form.validate())
-      return true;
+    if (form == null || !_formWasEdited || form.validate()) return true;
     return await showDialog<bool>(
-      context: context,
-      child: new AlertDialog(
-        title: const Text('This form has errors'),
-        content: const Text('Really leave this form?'),
-        actions: <Widget>[
-          new FlatButton(
-            child: const Text('YES'),
-            onPressed: () {
-              Navigator.of(context).pop(true);
-            },
+          context: context,
+          child: new AlertDialog(
+            title: const Text('This form has errors'),
+            content: const Text('Really leave this form?'),
+            actions: <Widget>[
+              new FlatButton(
+                child: const Text('YES'),
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+              ),
+              new FlatButton(
+                child: const Text('NO'),
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+              ),
+            ],
           ),
-          new FlatButton(
-            child: const Text('NO'),
-            onPressed: () {
-              Navigator.of(context).pop(false);
-            },
-          ),
-        ],
-      ),
-    ) ?? false;
+        ) ??
+        false;
+  }
+
+  getImage() async {
+    var fileName = await ImagePicker.pickImage();
+    setState(() {
+      event.imageFile = fileName;
+    });
   }
 
   //MARK: User Interface
+  Widget eventImageSection() {
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+            child: new Center(
+              child: new Column(
+                children: [
+                  new ClipOval(
+                    child: event.imageFile == null
+                        ? new Container(
+                            // alignment: Alignment.center,
+                            color: Colors.blueGrey,
+                            width: 150.0,
+                            height: 150.0,
+                            child: new IconButton(
+                              icon: new Icon(
+                                Icons.add_a_photo,
+                                color: Colors.white,
+                              ),
+                              onPressed: getImage,
+                            ),
+                          )
+                        : new Container(
+                            //  alignment: Alignment.center,
+                            width: 150.0,
+                            height: 150.0,
+                            child: new IconButton(
+                              icon: new Icon(
+                                Icons.add_a_photo,
+                                color: Colors.white,
+                              ),
+                              onPressed: getImage,
+                            ),
+                            decoration: new BoxDecoration(
+                              image: new DecorationImage(
+                                fit: BoxFit.cover,
+                                image: new FileImage(
+                                  event.imageFile,
+                                  scale: 0.25,
+                                ),
+                              ),
+                            ),
+                          ),
+                  ),
+                  new Container(
+                    margin: const EdgeInsets.only(top: 16.0),
+                    child: new Text(
+                      "Tap to update your event banner",
+                      style: new TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget formSection() {
+    return new Form(
+        key: _formKey,
+        autovalidate: _autovalidate,
+        onWillPop: _warnUserAboutInvalidData,
+        child: new Column(
+            children: <Widget>[
+              new TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'What do you want to call this event?',
+                  labelText: 'Title *',
+                ),
+                onSaved: (String value) {
+                  event.title = value;
+                },
+                validator: _validateTitle,
+              ),
+              new TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'What is this event about?',
+                    helperText: 'Make it flavorful!',
+                    labelText: 'About *',
+                  ),
+                  maxLines: 10,
+                  onSaved: (String value) {
+                    event.description = value;
+                  }
+                //TODO: No validator yet.
+              ),
+              new Container(
+                padding: const EdgeInsets.all(20.0),
+                //       Unclear     alignment: Alignment.center ,
+                child: new RaisedButton(
+                  child: const Text('SUBMIT'),
+                  onPressed: _handleSubmitted,
+                ),
+              ),
+              new Container(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: new Text('* indicates required field', style: Theme
+                    .of(context)
+                    .textTheme
+                    .caption),
+              ),
+            ]
+        )
+    );
+  }
+
+  //MARK: Build Screen
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(title: new Text('Create event'), centerTitle: true),
-        body: new Form(
-            key: _formKey,
-            autovalidate: _autovalidate,
-            onWillPop: _warnUserAboutInvalidData,
-            child: new ListView(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                children: <Widget>[
-                  new TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: 'What do you want to call this event?',
-                      labelText: 'Title *',
-                    ),
-                    onSaved: (String value) {
-                      event.title = value;
-                    },
-                    validator: _validateTitle,
-                  ),
-                  new TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: 'What is this event about?',
-                        helperText: 'Make it flavorful!',
-                        labelText: 'About *',
-                      ),
-                      maxLines: 10,
-                      onSaved: (String value) {
-                        event.description = value;
-                      }
-                    //TODO: No validator yet.
-                  ),
-                  new Container(
-                    padding: const EdgeInsets.all(20.0),
-                    //       Unclear     alignment: Alignment.center ,
-                    child: new RaisedButton(
-                      child: const Text('SUBMIT'),
-                      onPressed: _handleSubmitted,
-                    ),
-                  ),
-                  new Container(
-                    padding: const EdgeInsets.only(top: 20.0),
-                    child: new Text('* indicates required field', style: Theme
-                        .of(context)
-                        .textTheme
-                        .caption),
-                  ),
-                ]
-            )
-        )
+        body: _buildContent(context));
+  }
+
+  Widget _buildContent(BuildContext context) {
+    return new ListView(
+      padding: const EdgeInsets.all(16.0),
+      children: [
+        eventImageSection(),
+        formSection()
+      ],
     );
   }
 }
-
