1cde846a77c470db3c32da9e4cd7fe0b0fddca49
juntski <jonathan.granstroem@gmail.com>
2017-10-20 21:18:28 +0200

    Working match view

--- libraryapp/lib/matches.dart
+++ libraryapp/lib/matches.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "dart:io";
+import "dart:math";
 
 enum MatchesAction { reset, leftSwipe, rightSwipe, horizontal }
 
@@ -8,18 +10,28 @@ class Matches extends StatefulWidget {
 }
 
 class EventItem implements Comparable<EventItem> {
-  EventItem({this.index, this.name, this.subject, this.body});
+  static var rand = new Random();
+  EventItem({this.index, this.title, this.description, this.imageFile});
 
   EventItem.from(EventItem item)
       : index = item.index,
-        name = item.name,
-        subject = item.subject,
-        body = item.body;
+        title = item.title,
+        description = item.description,
+        imageFile = item.imageFile;
 
   final int index;
-  final String name;
-  final String subject;
-  final String body;
+  final String title;
+  final String description;
+  final File imageFile;
+
+    bool artToggle = rand.nextBool(),
+      bookToggle = rand.nextBool(),
+      cultureToggle = rand.nextBool(),
+      poetryToggle = rand.nextBool(),
+      appsToggle = rand.nextBool(),
+      filmToggle = rand.nextBool(),
+      natureToggle = rand.nextBool(),
+      languageToggle = rand.nextBool();
 
   @override
   int compareTo(EventItem other) => index.compareTo(other.index);
@@ -33,9 +45,10 @@ class MatchesState extends State<Matches> {
     eventItems = new List<EventItem>.generate(16, (int index) {
       return new EventItem(
           index: index,
-          name: "Event $index",
-          subject: "Subject: $index",
-          body: "The body of event [$index]...");
+          title: "Title: $index",
+          description:
+              "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          imageFile: null);
     });
   }
 
@@ -87,24 +100,324 @@ class MatchesState extends State<Matches> {
           eventItems.remove(item);
         });
         final String action = (direction == DismissDirection.endToStart)
-            ? "Accepted"
-            : "Not interested";
+            ? "Not interested"
+            : "Interested";
         Scaffold.of(context).showSnackBar(new SnackBar(
-          content: new Text("$action"),
-        ));
+              content: new Text("$action"),
+            ));
       },
       child: new Padding(
         padding: const EdgeInsets.all(16.0),
         child: new Card(
-          color: Colors.white,
-          child: new Center(
-            child: new Column(
-              children: [
-                new Text(item.name),
-                new Text(item.subject),
-                new Text(item.body),
-              ],
-            ),
+          child: new Column(
+            children: [
+              new ClipRect(
+                child: item.imageFile == null
+                    ? new Container(
+                        alignment: Alignment.center,
+                        color: Theme.of(context).disabledColor,
+                        height: 150.0,
+                      )
+                    : new Container(
+                        alignment: Alignment.center,
+                        decoration: new BoxDecoration(
+                          image: new DecorationImage(
+                            fit: BoxFit.cover,
+                            image: new FileImage(
+                              item.imageFile,
+                            ),
+                          ),
+                        ),
+                      ),
+              ),
+              new Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.all(16.0),
+                child: new Text(
+                  item.title,
+                  textAlign: TextAlign.start,
+                  style: new TextStyle(
+                    fontSize: 24.0,
+                  ),
+                ),
+              ),
+              new Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.only(left: 16.0, right: 16.0),
+                child: new Text(
+                  item.description,
+                  textAlign: TextAlign.start,
+                  style: new TextStyle(
+                    fontSize: 11.0,
+                  ),
+                ),
+              ),
+              new Container(
+                child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        new Container(
+                          margin: const EdgeInsets.only(top: 16.0, left: 16.0),
+                          child: new Text(
+                            "Interests",
+                            style: new TextStyle(
+                                color: Theme.of(context).primaryColor),
+                          ),
+                        ),
+                        new Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            new Center(
+                              child: new Column(
+                                children: [
+                                  new Container(
+                                    margin: const EdgeInsets.all(8.0),
+                                    child: new Column(
+                                      children: <Widget>[
+                                        new Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 8.0, bottom: 8.0),
+                                          child: new Icon(
+                                            Icons.palette,
+                                            size: 30.0,
+                                            color: item.artToggle
+                                                ? Theme.of(context).primaryColor
+                                                : Theme
+                                                    .of(context)
+                                                    .disabledColor,
+                                          ),
+                                        ),
+                                        new Text(
+                                          "Art & Music",
+                                          style: new TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 11.0,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  new Container(
+                                    margin: const EdgeInsets.all(8.0),
+                                    child: new Column(
+                                      children: <Widget>[
+                                        new Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 8.0, bottom: 8.0),
+                                          child: new Icon(
+                                            Icons.computer,
+                                            size: 30.0,
+                                            color: item.appsToggle
+                                                ? Theme.of(context).primaryColor
+                                                : Theme
+                                                    .of(context)
+                                                    .disabledColor,
+                                          ),
+                                        ),
+                                        new Text(
+                                          "Apps & Internet",
+                                          style: new TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 11.0,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            new Center(
+                              child: new Column(
+                                children: [
+                                  new Container(
+                                    margin: const EdgeInsets.all(8.0),
+                                    child: new Column(
+                                      children: <Widget>[
+                                        new Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 8.0, bottom: 8.0),
+                                          child: new Icon(
+                                            Icons.local_library,
+                                            size: 30.0,
+                                            color: item.bookToggle
+                                                ? Theme.of(context).primaryColor
+                                                : Theme
+                                                    .of(context)
+                                                    .disabledColor,
+                                          ),
+                                        ),
+                                        new Text(
+                                          "Book circles",
+                                          style: new TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 11.0,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  new Container(
+                                    margin: const EdgeInsets.all(8.0),
+                                    child: new Column(
+                                      children: <Widget>[
+                                        new Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 8.0, bottom: 8.0),
+                                          child: new Icon(
+                                            Icons.movie,
+                                            size: 30.0,
+                                            color: item.filmToggle
+                                                ? Theme.of(context).primaryColor
+                                                : Theme
+                                                    .of(context)
+                                                    .disabledColor,
+                                          ),
+                                        ),
+                                        new Text(
+                                          "Film",
+                                          style: new TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 11.0,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            new Center(
+                              child: new Column(
+                                children: [
+                                  new Container(
+                                    margin: const EdgeInsets.all(8.0),
+                                    child: new Column(
+                                      children: <Widget>[
+                                        new Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 8.0, bottom: 8.0),
+                                          child: new Icon(
+                                            Icons.school,
+                                            size: 30.0,
+                                            color: item.cultureToggle
+                                                ? Theme.of(context).primaryColor
+                                                : Theme
+                                                    .of(context)
+                                                    .disabledColor,
+                                          ),
+                                        ),
+                                        new Text(
+                                          "Culture & Education",
+                                          style: new TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 11.0,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  new Container(
+                                    margin: const EdgeInsets.all(8.0),
+                                    child: new Column(
+                                      children: <Widget>[
+                                        new Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 8.0, bottom: 8.0),
+                                          child: new Icon(
+                                            Icons.nature_people,
+                                            size: 30.0,
+                                            color: item.natureToggle
+                                                ? Theme.of(context).primaryColor
+                                                : Theme
+                                                    .of(context)
+                                                    .disabledColor,
+                                          ),
+                                        ),
+                                        new Text(
+                                          "Nature & Society",
+                                          style: new TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 11.0,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            new Center(
+                              child: new Column(
+                                children: [
+                                  new Container(
+                                    margin: const EdgeInsets.all(8.0),
+                                    child: new Column(
+                                      children: <Widget>[
+                                        new Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 8.0, bottom: 8.0),
+                                          child: new Icon(
+                                            Icons.edit,
+                                            size: 30.0,
+                                            color: item.poetryToggle
+                                                ? Theme.of(context).primaryColor
+                                                : Theme
+                                                    .of(context)
+                                                    .disabledColor,
+                                          ),
+                                        ),
+                                        new Text(
+                                          "Poetry & Prose",
+                                          style: new TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 11.0,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  new Container(
+                                    margin: const EdgeInsets.all(8.0),
+                                    child: new Column(
+                                      children: <Widget>[
+                                        new Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 8.0, bottom: 8.0),
+                                          child: new Icon(
+                                            Icons.language,
+                                            size: 30.0,
+                                            color: item.languageToggle
+                                                ? Theme.of(context).primaryColor
+                                                : Theme
+                                                    .of(context)
+                                                    .disabledColor,
+                                          ),
+                                        ),
+                                        new Text(
+                                          "Laguage",
+                                          style: new TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 11.0,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -116,12 +429,7 @@ class MatchesState extends State<Matches> {
       padding: const EdgeInsets.all(16.0),
       child: new Dismissible(
         key: new ObjectKey("yo"),
-        child: new Card(
-          color: Colors.white,
-          child: new Center(
-            child: new Text("MATCH BODY"),
-          ),
-        ),
+        child: new Card(),
         onDismissed: (DismissDirection direction) {
           setState(() {});
         },
