8be43f7dadbf86c6ffc819ab3447a5b18c90de79
Alotte <alma.ottedag@gmail.com>
2017-10-26 17:59:00 +0200

    made unecessary stuff, don't mind.

--- libraryapp/lib/presenter/profile_presenter.dart
+++ libraryapp/lib/presenter/profile_presenter.dart
@@ -1,4 +1,5 @@
 import 'package:libraryapp/data/user_data.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /*
  * The contract is the interface between the view and the presenter.
@@ -6,8 +7,9 @@ import 'package:libraryapp/data/user_data.dart';
  * to events that occur in the view and take action.
  */
 abstract class ProfileViewContract {
-  void onLoadProfileComplete(User user);
-  void onLoadProfileError();
+  void onLoadUserComplete(User user);
+  void onLoadUserError();
+  void saveUser(User user);
 }
 
 /*
@@ -18,9 +20,22 @@ abstract class ProfileViewContract {
 
 class ProfilePresenter {
   ProfileViewContract _view;
+  SharedPreferences _prefs;
   User user;
 
   ProfilePresenter(this._view) {
     //TODO: Store data and stuff.
+  }
+
+  loadUser() async {
+    assert(_view != null);
+    _prefs = await SharedPreferences.getInstance();
+    String name = _prefs.getString(UserKey.name);
+    String occupation = _prefs.getString(UserKey.occupation);
+    String year = _prefs.getString(UserKey.year);
+    String description = _prefs.getString(UserKey.description);
+    List<String> eventThemes = _prefs.getStringList(UserKey.eventThemes);
+    //TODO: Make the image file load somehow.
+    //File imageFile = new Uri.file(_prefs.getString(UserKey.imageFile))}
   }
 }
