f5c7b59337860212f64120a26b7aa75cf11a536f
Jonas Arvidsson <jonas.arvidsson0@gmail.com>
2017-10-29 14:45:58 +0100

    more event cards added

--- libraryapp/lib/tabs/created.dart
+++ libraryapp/lib/tabs/created.dart
@@ -3,10 +3,123 @@ import "package:flutter/material.dart";
 class Created extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      child: new Center(
-        child: new Text("CREATED"),
-      ),
-    );
+     return new Container(
+        child: new ListView(
+      padding: new EdgeInsets.all(8.0),
+      children: <Widget>[
+        //Column which contains all rows
+        new Column(children: [
+          //First row
+          new GestureDetector(
+            onTap: () {
+              openEvent();
+            },
+            child: 
+          new Container(
+            margin: new EdgeInsets.only(bottom: 10.0),
+            height: 100.0,
+            decoration: new BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              boxShadow: <BoxShadow>[
+                new BoxShadow(
+                  color: const Color(0xcc000000),
+                  offset: new Offset(0.0, 2.0),
+                  blurRadius: 2.0,
+                ),
+              ],
+            ),
+            child: new Row(
+              children: [
+                // Fox
+                new Image.asset(
+                  'images/puzzles.jpg',
+                  height: 100.0,
+                  width: 100.0,
+                  fit: BoxFit.cover,
+                ),
+                // Info section
+                new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    new Container(
+                      padding: const EdgeInsets.all(4.0),
+                      child: new Text(
+                        'PUZZLES',
+                        // textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: new TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            new Icon(Icons.date_range,
+                                size: 18.0, color: Colors.white),
+                            new Text(
+                              '4th June 2018',
+                              //textAlign: TextAlign.left,
+                              overflow: TextOverflow.ellipsis,
+                              style: new TextStyle(
+                                  color: Colors.white, fontSize: 10.0),
+                            )
+                          ]),
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            new Icon(Icons.access_time,
+                                size: 18.0, color: Colors.white),
+                            new Text(
+                              '08:00',
+                              //textAlign: TextAlign.left,
+                              overflow: TextOverflow.ellipsis,
+                              style: new TextStyle(
+                                  color: Colors.white, fontSize: 10.0),
+                            )
+                          ]),
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            new Container(
+                                child: new Row(
+                              children: [
+                                new Icon(Icons.location_city,
+                                    size: 18.0, color: Colors.white),
+                                new Text(
+                                  'Göteborgs \nStadsbibliotek',
+                                  //textAlign: TextAlign.left,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: new TextStyle(
+                                      color: Colors.white, fontSize: 10.0),
+                                )
+                              ],
+                            ))
+                          ]),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          ),
+          //second row
+          //end of row
+        ]),
+      ],
+    ));
   }
+}
+
+void openEvent() {
+   print("Container clicked");
 }
--- libraryapp/lib/tabs/upcoming.dart
+++ libraryapp/lib/tabs/upcoming.dart
@@ -5,18 +5,18 @@ import 'dart:io';
 class Upcoming extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-     return new GestureDetector(
-        onTap: (){
-        print("Container clicked");
-        },
-    child: new Container(
-      //onPressed: () { print("xD"); },
+     return new Container(
         child: new ListView(
       padding: new EdgeInsets.all(8.0),
       children: <Widget>[
         //Column which contains all rows
         new Column(children: [
           //First row
+          new GestureDetector(
+            onTap: () {
+              openEvent();
+            },
+            child: 
           new Container(
             margin: new EdgeInsets.only(bottom: 10.0),
             height: 100.0,
@@ -189,13 +189,300 @@ class Upcoming extends StatelessWidget {
               ],
             ),
           ),
+          ),
           //second row
+             new GestureDetector(
+            onTap: () {
+              openEvent();
+            },
+            child: 
+          new Container(
+            margin: new EdgeInsets.only(bottom: 10.0),
+            height: 100.0,
+            decoration: new BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              boxShadow: <BoxShadow>[
+                new BoxShadow(
+                  color: const Color(0xcc000000),
+                  offset: new Offset(0.0, 2.0),
+                  blurRadius: 2.0,
+                ),
+              ],
+            ),
+            child: new Row(
+              children: [
+                // Fox
+                new Image.asset(
+                  'images/knittingnight.jpg',
+                  height: 100.0,
+                  width: 100.0,
+                  fit: BoxFit.cover,
+                ),
+                // Info section
+                new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    new Container(
+                      padding: const EdgeInsets.all(4.0),
+                      child: new Text(
+                        'KNITTING',
+                        // textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: new TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            new Icon(Icons.date_range,
+                                size: 18.0, color: Colors.white),
+                            new Text(
+                              '14th Feb 2018',
+                              //textAlign: TextAlign.left,
+                              overflow: TextOverflow.ellipsis,
+                              style: new TextStyle(
+                                  color: Colors.white, fontSize: 10.0),
+                            )
+                          ]),
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            new Icon(Icons.access_time,
+                                size: 18.0, color: Colors.white),
+                            new Text(
+                              '11:30',
+                              //textAlign: TextAlign.left,
+                              overflow: TextOverflow.ellipsis,
+                              style: new TextStyle(
+                                  color: Colors.white, fontSize: 10.0),
+                            )
+                          ]),
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            new Container(
+                                child: new Row(
+                              children: [
+                                new Icon(Icons.location_city,
+                                    size: 18.0, color: Colors.white),
+                                new Text(
+                                  'Göteborgs \nStadsbibliotek',
+                                  //textAlign: TextAlign.left,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: new TextStyle(
+                                      color: Colors.white, fontSize: 10.0),
+                                )
+                              ],
+                            ))
+                          ]),
+                    ),
+                  ],
+                ),
+                // People section
+                new Container(
+                    //decoration: new BoxDecoration(color: Colors.blue),
+                    padding: new EdgeInsets.symmetric(
+                        vertical: 25.0, horizontal: 25.0),
+                    child: (new Row(
+                      children: [
+                        new Column(
+                          children: [
+                            new ClipOval(
+                              child: new Image.asset(
+                                'images/guy3.jpg',
+                                height: 30.0,
+                                width: 30.0,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            new Text("Steve",
+                                style: new TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12.0,
+                                )),
+                          ],
+                        ),
+                      ],
+                    )))
+              ],
+            ),
+          ),
+          ),
+           new Container(
+            margin: new EdgeInsets.only(bottom: 10.0),
+            height: 100.0,
+            decoration: new BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              boxShadow: <BoxShadow>[
+                new BoxShadow(
+                  color: const Color(0xcc000000),
+                  offset: new Offset(0.0, 2.0),
+                  blurRadius: 2.0,
+                ),
+              ],
+            ),
+            child: new Row(
+              children: [
+                // Fox
+                new Image.asset(
+                  'images/movenight.jpg',
+                  height: 100.0,
+                  width: 100.0,
+                  fit: BoxFit.cover,
+                ),
+                // Info section
+                new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    new Container(
+                      padding: const EdgeInsets.all(4.0),
+                      child: new Text(
+                        'MOVIE NIGHT',
+                        // textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: new TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            new Icon(Icons.date_range,
+                                size: 18.0, color: Colors.white),
+                            new Text(
+                              '25th May 2018',
+                              //textAlign: TextAlign.left,
+                              overflow: TextOverflow.ellipsis,
+                              style: new TextStyle(
+                                  color: Colors.white, fontSize: 10.0),
+                            )
+                          ]),
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            new Icon(Icons.access_time,
+                                size: 18.0, color: Colors.white),
+                            new Text(
+                              '23:00',
+                              //textAlign: TextAlign.left,
+                              overflow: TextOverflow.ellipsis,
+                              style: new TextStyle(
+                                  color: Colors.white, fontSize: 10.0),
+                            )
+                          ]),
+                    ),
+                    new Container(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            new Container(
+                                child: new Row(
+                              children: [
+                                new Icon(Icons.location_city,
+                                    size: 18.0, color: Colors.white),
+                                new Text(
+                                  'Göteborgs \nStadsbibliotek',
+                                  //textAlign: TextAlign.left,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: new TextStyle(
+                                      color: Colors.white, fontSize: 10.0),
+                                )
+                              ],
+                            ))
+                          ]),
+                    ),
+                  ],
+                ),
+                // People section
+                new Container(
+                    //decoration: new BoxDecoration(color: Colors.blue),
+                    padding: new EdgeInsets.symmetric(
+                        vertical: 25.0, horizontal: 5.0),
+                    child: (new Row(
+                      children: [
+                        new Column(
+                          children: [
+                            new ClipOval(
+                              child: new Image.asset(
+                                'images/guy4.png',
+                                height: 30.0,
+                                width: 30.0,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            new Text("Lasse",
+                                style: new TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12.0,
+                                )),
+                          ],
+                        ),
+                        new Column(
+                          children: [
+                            new ClipOval(
+                              child: new Image.asset(
+                                'images/guy5.jpg',
+                                height: 30.0,
+                                width: 30.0,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            new Text("Tom",
+                                style: new TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12.0,
+                                )),
+                          ],
+                        ),
+                        new Column(
+                          children: [
+                            new ClipOval(
+                              child: new Image.asset(
+                                'images/girl2.jpg',
+                                height: 30.0,
+                                width: 30.0,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            new Text("Lisa",
+                                //overflow: TextOverflow.ellipsis,
+                                style: new TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12.0,
+                                )),
+                          ],
+                        ),
+                      ],
+                    )))
+              ],
+            ),
+          ),
           //end of row
         ]),
       ],
-    ))
-    );
+    ));
   }
 }
 
-Widget getEvents() {}
+void openEvent() {
+   print("Container clicked");
+}
